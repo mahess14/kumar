b7039dc240aeb1ddd2d68a0e80cb1d25bfc65ef0
mahess14 <gmksvec14@gmail.com>
2023-03-30 18:15:35 +0530

    teting4 commit

--- src/test/java/gittcomm/test/Test4.java
+++ src/test/java/gittcomm/test/Test4.java
@@ -3,8 +3,8 @@ package gittcomm.test;
 public class Test4 {
 
 	public static void main(String[] args) {
-		int a=10;
-		int b=20;
+		int a=3:0;
+		int b=40;
 		System.out.println(a==b); //false
 		System.out.println(a<b);  // true
 		System.out.println(a>b);   // false
