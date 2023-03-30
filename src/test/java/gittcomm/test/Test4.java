6e92cc43324a154a8868d813b7f64758ea807ef5
mahess14 <gmksvec14@gmail.com>
2023-03-30 18:09:30 +0530

    teting4 commit

---
+++ src/test/java/gittcomm/test/Test4.java
@@ -0,0 +1,20 @@
+package gittcomm.test;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println(a==b); //false
+		System.out.println(a<b);  // true
+		System.out.println(a>b);   // false
+		System.out.println(a<=b);  // true
+		System.out.println(a>=b);  //false
+		System.out.println(a!=b);  //true
+
+	}
+
+
+	}
+
+
